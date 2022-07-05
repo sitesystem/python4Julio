9b34d4c4ac54b1e3f7080b82075ddee4e48c096c
sitesystem <l_aloaxa05@hotmail.com>
2022-07-04 19:53:11 -0500

    Primera subida de archivo Hola Mundo

---
+++ Curso Python4Julio/Hola_Mundo.py
@@ -0,0 +1,17 @@
+# EJEMPLO 1. Saludo
+# COMENTARIO.- Sirven para documentar o detallar cada parte del código
+# Los comentarios los omite o no los toma en cuenta Python
+
+'''
+Comentario: OPERACIONES MATEMÁTICAS Y SALUDO
+'''
+
+# Imprimir o mostrar resultados en pantalla
+print(2+5)
+print(10-3)
+print(8*7)
+print(40/5)
+
+# Imprimir saludo
+print("Hola Mundo")
+print('Jocelyn');
